4998a571ad01c489427fa92926223203a58528d2
Nitish2000 <nitishmani111@gmail.com>
2024-04-10 18:17:03 +0530

    Code updated

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget{
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context){
+
+    const border =  OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 20, 19, 19),
+                    width: 2,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignCenter,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(50))
+                );
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 201, 218, 203),
+      body : Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("0",
+            style: TextStyle(
+              fontSize: 50,
+              fontFamily:AutofillHints.creditCardExpirationDay,
+              fontWeight: FontWeight.w900,
+              color: Colors.blue
+            ),),
+            Padding(
+              padding: EdgeInsets.all(10.0);
+              child: TextField(
+                
+                style: TextStyle(
+                  color: Color.fromARGB(255, 38, 39, 35),
+                  fontSize: 30,
+                 ),
+                
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 240, 237, 237),
+                  hintText: "Please Enter amount",
+                  prefixIcon: Icon(Icons.currency_rupee),
+                  prefixIconColor: Color.fromARGB(255, 19, 5, 4) ,
+                  focusedBorder: border, // We called an variable
+                  enabledBorder:border,
+                  
+                  ),
+                  keyboardType: TextInputType.number,
+              
+                ),
+            ),
+          ],
+        ),
+      ) 
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'currency_converter_material_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//Types of widgets:
+//1. Stateless widgets   * no change of state happens
+//2. Stateful widgets    * change of state happens
+//3. inherited widgets   * know for functionality
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //We can return two types of app
+    //1. MaterialApp
+    //2. cupertinoApp
+    return const MaterialApp(
+        home: CurrencyConverterMaterialPage(),);
+  }
+}
